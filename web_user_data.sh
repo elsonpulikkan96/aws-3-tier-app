58638c6d3789545511818dcd368d604b5689442b
elsonpulikkan96 <elsonpulikkan@gmail.com>
2026-01-03 21:18:54 +0530

    aws-3-tier-app-full

--- web_user_data.sh
+++ web_user_data.sh
@@ -7,20 +7,20 @@ dnf install -y nginx git
 
 echo "========== Cloning application repository =========="
 cd /home/ec2-user
-git clone https://github.com/harishnshetty/terraform-project-vpc-alb-modules-workspace.git || true
+git clone https://github.com/elsonpulikkan96/aws-3-tier-app.git || true
 
 echo "========== Copying web.sh =========="
-cp -f /home/ec2-user/terraform-project-vpc-alb-modules-workspace/application_code/web.sh /home/ec2-user/web.sh
+cp -f /home/ec2-user/aws-3-tier-app/application_code/web.sh /home/ec2-user/web.sh
 chmod +x /home/ec2-user/web.sh
 
 echo "========== Preparing nginx.conf =========="
 # Replace placeholder BEFORE moving nginx.conf into /etc
 sed -i "s|REPLACE-WITH-INTERNAL-LB-DNS|__APP_ALB_DNS__|g" \
-    /home/ec2-user/terraform-project-vpc-alb-modules-workspace/application_code/nginx.conf
+    /home/ec2-user/aws-3-tier-app/application_code/nginx.conf
 
 # Backup old config & apply new one
 mv /etc/nginx/nginx.conf /etc/nginx/nginx-backup.conf || true
-cp -f /home/ec2-user/terraform-project-vpc-alb-modules-workspace/application_code/nginx.conf /etc/nginx/nginx.conf
+cp -f /home/ec2-user/aws-3-tier-app/application_code/nginx.conf /etc/nginx/nginx.conf
 
 echo "========== Running web.sh =========="
 /home/ec2-user/web.sh
